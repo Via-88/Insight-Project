8bbca98abcb45c7567858d62cd4377ccc3ee8ff7
Hongwei-Niu <hongweiniu0202@gmail.com>
2019-02-25 00:19:38 -0500

    new dashboards

--- web-app.py
+++ web-app.py
@@ -2,30 +2,30 @@ from flask import Flask, flash,url_for, redirect, render_template, request, sess
 
 import os
 #from flask_bootstrap import bootstrap
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/send')
-def send():
+@app.route('/head.html')
+def head_html():
     subject = request.args.get('subject')
-    output = ['condition',' size', 'color']
-    return render_template(
-            'send.html',
-            subject = subject,
-            output = output)
+    return render_template('head.html', subject=subject)
 
-@app.route('/output')
-def output():
-    subject = request.args.get('subject')
-    return render_template(
-            'output.html',
-            subject = subject
-            )
+@app.route('/pricemap.html')
+def pricemap_html():
+    return render_template('pricemap.html')
+
+@app.route('/adspool.html')
+def adspool_html():
+    return render_template('adspool.html')
+
+@app.route('/newoutput')
+def newoutput():
+    return render_template('newoutput.html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port, host='0.0.0.0', debug=True)
+    app.run(port=port, debug=True)
